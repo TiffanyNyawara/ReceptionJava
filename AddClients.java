117d6bdb3540bacd3515afc877f1e40b770430eb
TriadSyndicate <38569368+TriadSyndicate@users.noreply.github.com>
2018-09-28 19:52:23 +0300

    Add files via upload

--- AddClients.java
+++ AddClients.java
@@ -1,12 +1,21 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Date;
+import java.text.ParseException;
 import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+import reeption.DBUpdater;
 import reeption.Reeption;
 
 public class AddClients implements ActionListener{
       JFrame myframe = new JFrame("Add Clients");
       JPanel mypanel = new JPanel();
+      // Table Panels
+      JPanel tablePanel = new javax.swing.JPanel();
+      JScrollPane scrollPanel = new javax.swing.JScrollPane();
       //JObjects
         JLabel firstnamelabel = new JLabel("First Name: ");
         JLabel lastnamelabel = new JLabel("Last Name: ");
@@ -17,27 +26,35 @@ public class AddClients implements ActionListener{
         JTextField firstname = new JTextField();
         JTextField lastname = new JTextField();
         JTextField phone = new JTextField();
-        JTextField dob = new JTextField();
+        SimpleDateFormat dob = new SimpleDateFormat("yyyy-MM-dd");
+        com.toedter.calendar.JDateChooser jDate = new com.toedter.calendar.JDateChooser();
         JRadioButton genderm = new JRadioButton("Male");
         JRadioButton genderf = new JRadioButton("Female");
-        JButton submit = new JButton("Submit");
-        
-        String insert = null;
+        ButtonGroup group = new ButtonGroup();
+        JButton addbtn = new JButton("Add");
+        JButton retrievebtn = new JButton("Retrieve");
+        JButton updatebtn = new JButton("Update");
+        JButton deletebtn = new JButton("Delete");
+        JButton clearbtn = new JButton("Clear");
+        //Table***************************************************************************
+        JTable dataTable = new javax.swing.JTable();
+        //********************************************************************************
+        String infn,inln,inpn,indt = null;
         String gender = null;
-        String date_s = null;
+        String date = null;
+        Date t;
  
         int age = 0;
         Reeption x = new Reeption();
         
         AddClients(){
             GUI();
-            Submit();
+            
         }
       private void GUI(){
         firstname.setColumns(12);
         lastname.setColumns(12);
         phone.setColumns(12);
-        dob.setColumns(12);
         
         mypanel.setLayout(null);
         mypanel.add(firstnamelabel);
@@ -47,17 +64,67 @@ public class AddClients implements ActionListener{
         mypanel.add(phonelabel);
         mypanel.add(phone);
         mypanel.add(doblabel);
-        mypanel.add(dob);
+        mypanel.add(jDate);
         mypanel.add(genderlabel);
         mypanel.add(genderm);
         mypanel.add(genderf);
-        mypanel.add(submit);
+        mypanel.add(addbtn);
+        mypanel.add(retrievebtn);
+        mypanel.add(deletebtn);
+        mypanel.add(updatebtn);
+        mypanel.add(clearbtn);
+        mypanel.add(scrollPanel);
+        
+        mypanel.setBackground(new java.awt.Color(45, 155, 193));
+        
+        dataTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+
+            }
+        ));
+        
+        //Action Listeners
+         dataTable.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                dataTableMouseClicked(evt);
+            }
+        });
+         
+         //Button Actions
+        
+        retrievebtn.addActionListener((java.awt.event.ActionEvent evt) -> {
+            retrieveBtnActionPerformed(evt);
+        });
+
+        addbtn.addActionListener((java.awt.event.ActionEvent evt) -> {
+            addBtnActionPerformed(evt);
+        });
+            
+        updatebtn.addActionListener((java.awt.event.ActionEvent evt) -> {
+            updateBtnActionPerformed(evt);
+        });
+
+        deletebtn.addActionListener((java.awt.event.ActionEvent evt) -> {
+            DeleteActionPerformed(evt);
+        });
+            
+        clearbtn.addActionListener((java.awt.event.ActionEvent evt) -> {
+            clearBtnActionPerformed(evt);
+        });
+
+         
+        scrollPanel.setViewportView(dataTable);
         
         genderf.setActionCommand("Female");
         genderm.setActionCommand("Male");
-        ButtonGroup group = new ButtonGroup();
+        
         group.add(genderf);
         group.add(genderm);
+      
         
         firstnamelabel.setBounds(10, 70, 80, 20);
         firstname.setBounds(100, 70, 120, 20);
@@ -69,45 +136,179 @@ public class AddClients implements ActionListener{
         phone.setBounds(100, 130, 120, 20);
         
         doblabel.setBounds(10, 160, 100, 20);
-        dob.setBounds(100, 160, 120, 20);
+        jDate.setBounds(100, 160, 120, 20);
         
         genderlabel.setBounds(10, 190, 80, 20);
         genderm.setBounds(100, 190, 120, 20);
         genderf.setBounds(100, 220, 120, 20);
         
-        submit.setBounds(250, 80, 200, 150);
-        dob.setText("yyyy-mm-dd");
+        addbtn.setBounds(10, 260, 100, 25);
+        retrievebtn.setBounds(160, 260, 100, 25);
+        updatebtn.setBounds(10, 300, 100, 25);
+        deletebtn.setBounds(160, 300, 100, 25);
+        clearbtn.setBounds(85, 340, 100, 25);
+        
+        scrollPanel.setBounds(270, 70, 650, 400);
         myframe.add(mypanel);
-        myframe.setSize(800, 400);
+        
+        myframe.setSize(1000, 600);
         myframe.setVisible(true);
         myframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       }
-      private void Submit(){
-          submit.addActionListener(this);
+      /*private void Submit(){
+          addbtn.addActionListener(this);
       }
       
       @Override
-      public void actionPerformed(ActionEvent e){
+      public void actionPerformed(ActionEvent e){       
           if(genderm.isSelected() == true){
             gender = "Male";    
-             Date birth = Date.valueOf(dob.getText());
-             int xy = birth.getYear();
-             xy = 2018 - xy-1900;
-            insert = "insert into clientdetails(firstname,lastname,dateofbirth,gender,age)"+"Values('"+firstname.getText()+"','"+lastname.getText()+"','"+dob.getText()+"','"+gender+"','"+xy+"'";
-              x.InsertDB(insert);
-              System.out.print(insert);
+            String date = dob.format(jDate.getDate());
+            int a = jDate.getDate().getYear();
+            a = 2018 - a - 1900;
+            
+            insert = "insert into clientdetails(firstname,lastname,dateofbirth,gender,age)"+"Values('"+firstname.getText()+"','"+lastname.getText()+"','"+date+"','"+gender+"','"+a+"')";
+             x.ExecuteDB(insert);
+              //System.out.print(date + x);
         }
         
         if(genderf.isSelected() == true){
             gender = "Female";
-            Date birth = Date.valueOf(dob.getText());
-            int xy = birth.getYear();
-            xy = 2018 - xy-1900;
-            insert = "insert into clientdetails(firstname,lastname,dateofbirth,gender,age)"+"Values('"+firstname.getText()+"','"+lastname.getText()+"','"+dob.getText()+"','"+gender+"','"+xy+"'";
-            x.InsertDB(insert);
+            date = dob.format(jDate.getDate());
+            int a = jDate.getDate().getYear();
+            a = 2018 - a - 1900;
+            
+            insert = "insert into clientdetails(firstname,lastname,dateofbirth,gender,age)"+"Values('"+firstname.getText()+"','"+lastname.getText()+"','"+date+"','"+gender+"','"+a+"')";
+            x.ExecuteDB(insert);
         }
-      }
-    public static void main(String[] args){
-       new AddClients();
+      }*/
+        private void retrieve()
+    {
+     DefaultTableModel dm = new DBUpdater().getData();
+
+        dataTable.setModel(dm);
     }
+      
+          //RETRIEVE BUTTON CLICKED
+    private void retrieveBtnActionPerformed(java.awt.event.ActionEvent evt) {
+        retrieve();
+
+    }
+
+    //ADD OR SAVE
+    private void addBtnActionPerformed(java.awt.event.ActionEvent evt) {
+        if(genderm.isSelected() == true)
+            gender = "Male";   
+        if(genderf.isSelected() == true)
+            gender = "Female";
+        
+        age = jDate.getDate().getYear();
+            age = 2018 - age - 1900;
+        infn = firstname.getText();
+        inln = lastname.getText();
+        inpn = phone.getText();
+        indt = dob.format(jDate.getDate());
+            
+        if (new DBUpdater().add(infn, inln,inpn , indt, gender, age)) {
+            JOptionPane.showMessageDialog(null, "Successfully Inserted");
+
+            //CLEAR TXT
+            firstname.setText("");
+            lastname.setText("");
+            phone.setText("");         
+ 
+            retrieve();
+        } else {
+            JOptionPane.showMessageDialog(null, "Not Saved");
+        }
+    }
+
+    //SET SELECTED VALUE TO TEXTFIELDS
+    private void dataTableMouseClicked(java.awt.event.MouseEvent evt) {
+        String fname = dataTable.getValueAt(dataTable.getSelectedRow(), 1).toString();
+        String lname = dataTable.getValueAt(dataTable.getSelectedRow(), 2).toString();
+        String pnumber = dataTable.getValueAt(dataTable.getSelectedRow(), 3).toString();
+        String birthdate = dataTable.getValueAt(dataTable.getSelectedRow(), 4).toString();
+        String gendersex = dataTable.getValueAt(dataTable.getSelectedRow(), 5).toString();
+      
+        firstname.setText(fname);
+        lastname.setText(lname);
+        phone.setText(pnumber);
+        if("Female".equals(gendersex))
+            group.setSelected(genderf.getModel(), true);
+        else
+            group.setSelected(genderm.getModel(), true);
+        
+          try {
+              jDate.setDate(dob.parse(birthdate));
+          } catch (ParseException ex) {
+              Logger.getLogger(AddClients.class.getName()).log(Level.SEVERE, null, ex);
+          }
+    }
+
+    //UPDATE
+    private void updateBtnActionPerformed(java.awt.event.ActionEvent evt) {
+        int index = dataTable.getSelectedRow();
+        String id = dataTable.getValueAt(index, 0).toString();
+        if(genderm.isSelected() == true)
+            gender = "Male";   
+        if(genderf.isSelected() == true)
+            gender = "Female";
+        
+        age = jDate.getDate().getYear();
+            age = 2018 - age - 1900;
+
+        if (new DBUpdater().update(id, firstname.getText(), lastname.getText(), phone.getText(), (dob.format(jDate.getDate())),gender,age)) {
+            JOptionPane.showMessageDialog(null, "Successfully Updated");
+
+            //CLEAR TXT
+           firstname.setText("");
+           lastname.setText("");
+           phone.setText("");
+
+            retrieve();
+        } else {
+            JOptionPane.showMessageDialog(null, "Not Updated");
+        }
+    }
+
+    //DELETE
+    private void DeleteActionPerformed(java.awt.event.ActionEvent evt) {
+        String[] options = {"Yes", "No"};
+        int answ = JOptionPane.showOptionDialog(null, "Sure To Delete??", "Delete Confirm", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
+
+        if (answ == 0) {
+            int index = dataTable.getSelectedRow();
+            String id = dataTable.getValueAt(index, 0).toString();
+
+            if (new DBUpdater().delete(id)) {
+                JOptionPane.showMessageDialog(null, "Deleted Updated");
+
+                //CLEAR TXT
+                firstname.setText("");
+                lastname.setText("");
+                phone.setText("");
+
+           retrieve();
+            } else
+            {
+                JOptionPane.showMessageDialog(null, "Not Deleted");
+            }
+
+        }
+    }
+//CLEAR BUTTON CLICKED
+    private void clearBtnActionPerformed(java.awt.event.ActionEvent evt) {
+          dataTable.setModel(new DefaultTableModel());
+    }
+    public static void main(String[] args){
+          AddClients addClients = new AddClients();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
 }
---
+++ BarChart.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reeption;
+
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.RefineryUtilities;
+
+/**
+ *
+ * @author triad
+ */
+public class BarChart extends JFrame{
+    DBUpdater db = new DBUpdater();
+    int females, total, males;
+    int age, ageBracket1, ageBracket2, ageBracket3, ageBracket4;
+    
+    public BarChart(String title){
+        CategoryDataset dataset2  = createDataset2();
+        JFreeChart chart2 = createBarChart(dataset2, "Age Distribution");
+        ChartPanel chartPanel2 = new ChartPanel(chart2);
+        chartPanel2.setPreferredSize(new java.awt.Dimension(500, 300));
+        setContentPane(chartPanel2);
+    }
+     private CategoryDataset createDataset2(){
+              try {
+            //Male and Female Counter
+            while(db.chartData().next()){               
+                String gendersex = db.chartData().getString(1);
+                if("Female".equals(gendersex))
+                    females = females + 1;
+                if("Male".equals(gendersex))
+                    males = males + 1;
+                String ages = db.chartData().getString(2);
+                age = Integer.valueOf(ages);
+                if(age <= 18)
+                    ageBracket1 = ageBracket1 + 1;
+                if(age>18 && age <=25)
+                    ageBracket2 = ageBracket2 + 1;
+                if(age>25 && age<=35)
+                    ageBracket3 = ageBracket3 + 1;
+                if(age>35)
+                    ageBracket4 = ageBracket4 + 1;
+    }} catch (SQLException ex) {
+    Logger.getLogger(CreateChart.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        DefaultCategoryDataset res = new DefaultCategoryDataset();
+        res.addValue(ageBracket1, "0 - 18","");
+        res.addValue(ageBracket2, "18 - 25","");
+        res.addValue(ageBracket3, "25 - 35","");
+        res.addValue(ageBracket4, "35 and Over","");
+        return res;
+    }
+    private JFreeChart createBarChart(CategoryDataset data, String title){
+        JFreeChart chart2 = ChartFactory.createBarChart(title, "Bracket", "Number", data);
+        chart2.setBackgroundPaint(Color.WHITE);
+        CategoryPlot plot2 = chart2.getCategoryPlot();
+        plot2.setBackgroundPaint(Color.LIGHT_GRAY);
+        plot2.setDomainGridlinePaint(Color.WHITE);
+        plot2.setRangeGridlinePaint(Color.WHITE);
+        NumberAxis rangeAxis = (NumberAxis) plot2.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        
+        BarRenderer renderer = (BarRenderer) plot2.getRenderer();
+        GradientPaint gp0 = new GradientPaint(0.0f, 0.0f, Color.blue, 0.0f, 0.0f, Color.lightGray);
+        GradientPaint gp1 = new GradientPaint(0.0f, 0.0f, Color.green, 0.0f, 0.0f, Color.lightGray);
+        GradientPaint gp2 = new GradientPaint(0.0f, 0.0f, Color.red, 0.0f, 0.0f, Color.lightGray);
+	renderer.setSeriesPaint(0, gp0);
+	renderer.setSeriesPaint(1, gp1);
+	renderer.setSeriesPaint(2, gp2);
+        
+        CategoryAxis domainAxis = plot2.getDomainAxis();
+        domainAxis.setCategoryLabelPositions(CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 6.0));
+        return chart2;
+    }
+    public static void main(String[] args) {
+        BarChart cc = new BarChart("Age Distribution");
+        cc.pack();
+        RefineryUtilities.centerFrameOnScreen(cc);
+        cc.setVisible(true);
+    }
+}
